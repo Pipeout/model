82a65450e8c3a455326e6f5cff8e4222fbb4bdad
aurelio <aurelioserafim2807@gmail.com>
2026-05-23 17:44:44 -0300

    corrected route for api

--- scripts/api.py
+++ scripts/api.py
@@ -7,7 +7,7 @@ app = FastAPI()
 TRAINING_FILE = "scripts/ensemble.py"
 
 
-@app.post("/train")
+@app.post("/training")
 def train():
     subprocess.Popen(["python", TRAINING_FILE])
     return {"status": "training started"}
