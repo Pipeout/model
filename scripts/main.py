55d8f94e28ad7c10609dd2982fe3161dd2a93601
aurelio <aurelioserafim2807@gmail.com>
2026-06-29 17:24:06 -0300

    running random forest as the default model

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,6 @@
-import os
 import subprocess
-PATH_DESIRED_MODEL = 'svm.py'
+
+PATH_DESIRED_MODEL = "scripts/randomforest.py"
 if __name__ == "__main__":
     p1 = subprocess.Popen(["python", PATH_DESIRED_MODEL])
     p1.wait()
