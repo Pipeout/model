1e1cfb28fb75a2d6bfdbdd05526a6c50281eeb11
aurelio <aurelioserafim2807@gmail.com>
2026-07-01 14:12:28 -0300

    new model to run

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,6 @@
 import subprocess
 
-PATH_DESIRED_MODEL = "scripts/randomforest.py"
+PATH_DESIRED_MODEL = "scripts/xgbOptimizedThreshold.py"
 if __name__ == "__main__":
     p1 = subprocess.Popen(["python", PATH_DESIRED_MODEL])
     p1.wait()
